42fd0a1d8a28f23a7ac2e4a5112e7a5cd00bfd41
sebastianjmacharette <dev@macharette.com.ar>
2024-06-18 01:57:28 -0300

    resubida

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,14 +5,15 @@ import Logo from "../assets/logos/logo.png";
 function About() {
   return (
     <div id="about" className="min-h-screen flex flex-wrap bg-slate-100 relative">
- <div className="inline-flex lg:mt-0 sm:mt-4 items-center justify-center w-full">
-    <hr className="w-full h-1   border-2  bg-purple-700"/>
-    <span className="absolute px-3 font-medium text-purple-700 text-xl -translate-x-1/2 bg-white left-1/2 ">Sobre mi</span>
-</div>
-      <div className="flex flex-col items-center sm:mt-10 lg:mt-0 justify-center lg:w-1/2 w-full   pt-1 pb-1">
+      <div className="inline-flex lg:mt-0 sm:mt-4 items-center justify-center w-full">
+        <hr className="w-full h-1 border-2 bg-purple-700" />
+        <span className="absolute px-3 font-medium text-purple-700 text-xl -translate-x-1/2 bg-white left-1/2">
+          Sobre mi
+        </span>
+      </div>
+      <div className="flex flex-col items-center sm:mt-10 lg:mt-0 justify-center lg:w-1/2 w-full pt-1 pb-1">
         {/* Contenido del primer div superior */}
-
-        <img className="h-64 p-4 " src={Perfil} alt="Perfil" />
+        <img className="h-64 p-4" src={Perfil} alt="Perfil" />
         <a target="_blank" href="https://www.tiantech.com.ar">
           <p className="text-xl italic text-purple-600 font-semibold">
             Co-fundador de Tiantech+
@@ -22,25 +23,38 @@ function About() {
       <div className="flex flex-col items-center justify-center lg:w-1/2 w-full p-4 pt-0 pb-1">
         {/* Contenido del segundo div superior */}
         <p className="text-2xl text-center lg:leading-loose sm:leading-normal text-purple-600">
-          ¡Hola! <span className="font-semi-bold italic" >Soy Sebastián Macharette, Programador Full Stack </span>con una fuerte
-          especialización en Frontend y Diseño UI/UX. Me apasiona transformar tus
-          ideas y sueños digitales en soluciones concretas y funcionales.
+          ¡Hola!{" "}
+          <span className="font-semi-bold italic">
+            Soy Sebastián Macharette, Programador Full Stack{" "}
+          </span>
+          con una fuerte especialización en Frontend y Diseño UI/UX. Me apasiona
+          transformar tus ideas y sueños digitales en soluciones concretas y
+          funcionales.
         </p>
       </div>
-      <div className="flex flex-col items-center justify-center lg:w-1/2 w-full p-4 pt-1 pb-0 lg:-mt-8">
+      <div className="flex flex-col items-center justify-center lg:w-1/2 w-full p-4 pt-1 pb-0">
         {/* Contenido del primer div inferior */}
         <p className="text-2xl text-center lg:leading-relaxed sm:leading-normal text-zinc-600">
-          <span className="font-bold italic text-purple-600" >Mi misión </span>es llevar tus visiones al siguiente nivel, utilizando las
-          últimas tecnologías y las mejores prácticas de la industria. Desde la
+          <span className="font-bold italic text-purple-600">Mi misión </span>es
+          llevar tus visiones al siguiente nivel, utilizando las últimas
+          tecnologías y las mejores prácticas de la industria. Desde la
           conceptualización hasta la implementación, trabajo estrechamente
           contigo para asegurar que cada detalle se alinee con tu visión y
           objetivos.
         </p>
       </div>
-      <div className="flex flex-col lg:flex-row items-center justify-center lg:w-1/2 w-full p-4 pt-1 pb-0 lg:absolute lg:top-0 lg:right-0 lg:mt-80">
+      <div className="flex flex-col lg:flex-row items-center justify-center lg:w-1/2 w-full p-4 pt-1 pb-0  lg:mt-0 lg:flex lg:items-start">
         {/* Contenido del segundo div inferior */}
-      
-      
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/gep4ZEmpFvo?si=Kg6ANKB29KLLRxDZ"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
   );
--- src/studies/Skills.jsx
+++ src/studies/Skills.jsx
@@ -1,41 +1,10 @@
 import React, { useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
-import One from "../assets/skills/Image1.png";
-import Two from "../assets/skills/Image2.png";
-import Tree from "../assets/skills/Image3.png";
-import Four from "../assets/skills/Image4.png";
-import Five from "../assets/skills/Image5.png";
-import Six from "../assets/skills/Image6.png";
-import Seven from "../assets/skills/Image7.png";
-import Eight from "../assets/skills/Image8.png";
-import Nine from "../assets/skills/Image9.png";
-import Ten from "../assets/skills/Image10.png";
-import Eleven from "../assets/skills/Image11.png";
-import Twelve from "../assets/skills/Image12.png";
-import Thirteen from "../assets/skills/Image13.png";
-import Fourteen from "../assets/skills/Image14.png";
-import Fifteen from "../assets/skills/Image15.png";
-import Sixteen from "../assets/skills/Image16.png";
-import Seventeen from "../assets/skills/Image17.png";
-import Eighteen from "../assets/skills/Image18.png";
-import Nineteen from "../assets/skills/Image19.png";
-import Twenty from "../assets/skills/Image20.png";
-import TwentyOne from "../assets/skills/Image21.png";
-import TwentyTwo from "../assets/skills/Image22.png";
-import TwentyThree from "../assets/skills/Image23.png";
-import TwentyFour from "../assets/skills/Image24.png";
-import TwentyFive from "../assets/skills/Image25.png";
-import TwentySix from "../assets/skills/Image26.png";
-import TwentySeven from "../assets/skills/Image27.png";
-import TwentyEight from "../assets/skills/Image28.png";
-import TwentyNine from "../assets/skills/Image29.png";
-import Thirty from "../assets/skills/Image30.png";
-import ThirtyOne from "../assets/skills/Image31.png";
-import ThirtyTwo from "../assets/skills/Image32.png";
-import ThirtyThree from "../assets/skills/Image33.png";
-import ThirtyFour from "../assets/skills/Image34.png";
-import ThirtyFive from "../assets/skills/Image35.png";
+import One from "../assets/skills/1.png";
+import Two from "../assets/skills/2.png";
+import Tree from "../assets/skills/3.png";
+
 
 
 
@@ -45,39 +14,8 @@ function Skills() {
     const images = [
         { src: One },
         { src: Two },
-        { src: Tree },
-        { src: Four },
-        { src: Five },
-        { src: Six },
-        { src: Seven },
-        { src: Eight },
-        { src: Nine },
-        { src: Ten },
-        { src: Eleven },
-        { src: Twelve },
-        { src: Thirteen },
-        { src: Fourteen },
-        { src: Fifteen },
-        { src: Sixteen },
-        { src: Seventeen },
-        { src: Eighteen },
-        { src: Nineteen },
-        { src: Twenty },
-        { src: TwentyOne },
-        { src: TwentyTwo },
-        { src: TwentyThree },
-        { src: TwentyFour },
-        { src: TwentyFive },
-        { src: TwentySix },
-        { src: TwentySeven },
-        { src: TwentyEight },
-        { src: TwentyNine },
-        { src: Thirty },
-        { src: ThirtyOne },
-        { src: ThirtyTwo },
-        { src: ThirtyThree },
-        { src: ThirtyFour },
-        { src: ThirtyFive }
+        { src: Tree }
+        
     ];
     
 
