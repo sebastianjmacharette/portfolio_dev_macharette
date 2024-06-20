7ede7f8a200b55e923332aa2216df9fc1987ee2a
sebastianjmacharette <dev@macharette.com.ar>
2024-06-20 00:36:49 -0300

    resubida

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,39 +1,28 @@
 import React from "react";
 import Heros from "../assets/logos/hero.webp";
-import { Link } from "react-router-dom";
- function Hero() {
-  return (
-    <div
-    id="hero"
-    className="h-screen"
-    >
-             
 
-      <div
-        className="relative bg-cover bg-center h-screen "
-        style={{ backgroundImage: `url(${Heros})` }}
-      >
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white z-10">
-          {/* <img src="ruta/a/la/imagen.png" alt="Imagen del Hero" className="mx-auto mb-4 w-48 rounded-full" /> */}
-
-          <h1 className="text-4xl text-purple-500 font-bold mb-4 lg:text-right sm:text-center">
-            Sebastían J. Macharette
-          </h1>
-          <h2 className="text-2xl text-purple-400 font-bold mb-4 lg:text-right sm:text-center">
-            Full Stack Developer (Especializado en FrontEnd)
-          </h2>
-          <h2 className="text-2xl text-purple-500 font-bold mb-4 lg:text-right sm:text-center">
-            Diseñador UX/UI
-          </h2>
-           
-          
-          
+function Hero() {
+    return (
+        <div id="hero" className="h-screen relative">
+            <div
+                className="bg-cover bg-center h-full w-full"
+                style={{ backgroundImage: `url(${Heros})` }}
+            >
+                <div className="absolute inset-0 bg-black opacity-50"></div>
+                <div className="absolute inset-0 flex flex-col items-center justify-center text-white z-10">
+                    <h1 className="text-4xl text-purple-500 font-bold mb-4 text-center md:text-5xl lg:text-6xl">
+                        Sebastían J. Macharette
+                    </h1>
+                    <h2 className="text-2xl text-purple-400 font-bold mb-4 text-center md:text-3xl lg:text-4xl">
+                        Full Stack Developer (Especializado en FrontEnd)
+                    </h2>
+                    <h2 className="text-2xl text-purple-500 font-bold mb-4 text-center md:text-3xl lg:text-4xl">
+                        Diseñador UX/UI
+                    </h2>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Hero;
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";  
 import '../assets/Navbarcss.css'
 import Logo from '../assets/logos/logowhite.png'
+
 function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
@@ -25,6 +26,7 @@ function Navbar() {
             window.removeEventListener("scroll", handleScroll);
         };
     }, []);
+
     const navbarRef = useRef(null);
 
     useEffect(() => {
@@ -38,10 +40,12 @@ function Navbar() {
                 window.scrollTo({ top: offsetTop, behavior: 'smooth' });
             }
         };
+
         const links = document.querySelectorAll('nav a[href^="#"]');
         links.forEach(link => {
             link.addEventListener('click', handleLinkClick);
         });
+
         return () => {
             links.forEach(link => {
                 link.removeEventListener('click', handleLinkClick);
@@ -50,139 +54,43 @@ function Navbar() {
     }, []);
 
     return (
-        <div>
-            <nav ref={navbarRef}  className={`navbar bg-purple-600 ${isScrolled ? 'bg-opacity-70' : ''} ${isScrolled ? 'shadow-md' : ''} ${isScrolled ? 'fixed top-0' : ''} w-full`}>
+        <div className="w-full px-2">
+            <nav ref={navbarRef} className={`navbar bg-purple-600 ${isScrolled ? 'bg-opacity-70 shadow-md fixed top-0' : ''} w-full`}>
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a
-                        href="https://flowbite.com/"
-                        className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img
-                            src={Logo}
-                            className="w-20"
-                            alt="Flowbite Logo"
-                        />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap  text-white">
-                            Porfolio Digital
-                        </span>
+                    <a href="/" className="flex items-center space-x-3">
+                        <img src={Logo} className="w-20" alt="Portfolio Logo" />
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Portfolio Digital</span>
                     </a>
                     <button
                         onClick={toggleMenu}
                         type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm
-                             text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         aria-controls="navbar-default"
                         aria-expanded={menuOpen ? "true" : "false"}>
-                        <span className="sr-only">
-                            {menuOpen ? "Close main menu" : "Open main menu"}
-                        </span>
-                        <svg
-                            className="w-5 h-5"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 17 14">
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d={menuOpen ? "M5 3l6 6m0 0l-6 6m6-6H0" : "M1 1h15M1 7h15M1 13h15"}
-                            />
+                        <span className="sr-only">{menuOpen ? "Close main menu" : "Open main menu"}</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={menuOpen ? "M5 3l6 6m0 0l-6 6m6-6H0" : "M1 1h15M1 7h15M1 13h15"} />
                         </svg>
                     </button>
-                    <div className={menuOpen ? "block w-full md:hidden" : "hidden w-full md:hidden"} id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 mt-4 border border-gray-100 rounded-lg">
+                    <div className={`${menuOpen ? "block" : "hidden"} w-full md:flex md:w-auto`} id="navbar-default">
+                        <ul className="font-medium flex flex-col md:flex-row md:space-x-8 p-4 md:p-0 mt-4 md:mt-0 border border-gray-100 md:border-0 rounded-lg md:rounded-none bg-purple-600 md:bg-transparent">
                             <li>
-                                <a
-                                    href="#hero"
-                                    className="block py-2 px-3 text-white rounded"
-                                    aria-current="page">
-                                    Home
-                                </a>
+                                <a href="#hero" className="block py-2 px-3 text-white rounded md:p-0" aria-current="page">Home</a>
                             </li>
                             <li>
-                                <a
-                                    href="#about"
-                                    className="block py-2 px-3 text-white rounded">
-                                    Sobre mi
-                                </a>
+                                <a href="#about" className="block py-2 px-3 text-white rounded md:p-0">Sobre mi</a>
                             </li>
                             <li>
-                                <a
-                                    href="#study"
-                                    className="block py-2 px-3 text-white rounded">
-                                    Estudios
-                                </a>
-                            </li>
-                          
-                            <li>
-                                <a
-                                    href="#proyects"
-                                    className="block py-2 px-3 text-white rounded">
-                                    Proyectos
-                                </a>
+                                <a href="#study" className="block py-2 px-3 text-white rounded md:p-0">Estudios</a>
                             </li>
                             <li>
-                                <a
-                                    href="#clients"
-                                    className="block py-2 px-3 text-white rounded">
-                                    Clientes
-                                </a>
+                                <a href="#proyects" className="block py-2 px-3 text-white rounded md:p-0">Proyectos</a>
                             </li>
                             <li>
-                                <a
-                                    href="#contact"
-                                    className="block py-2 px-3 text-white rounded">
-                                    Contacto
-                                </a>
-                            </li> 
-                        </ul>
-                    </div>
-                    <div className="hidden w-full md:flex md:w-auto" id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-                            <li>
-                                <a
-                                    href="#hero"
-                                    className="block py-2 px-3 text-white rounded md:p-0 dark:text-white"
-                                    aria-current="page">
-                                    Home
-                                </a>
+                                <a href="#clients" className="block py-2 px-3 text-white rounded md:p-0">Clientes</a>
                             </li>
                             <li>
-                                <a
-                                    href="#about"
-                                    className="block py-2 px-3 text-white rounded md:p-0 dark:text-white">
-                                    Sobre mi
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#study"
-                                    className="block py-2 px-3 text-white rounded md:p-0 dark:text-white">
-                                    Estudios
-                                </a>
-                            </li>
-                         
-                            <li>
-                                <a
-                                    href="#proyects"
-                                    className="block py-2 px-3 text-white rounded md:p-0 dark:text-white">
-                                    Proyectos
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#clients"
-                                    className="block py-2 px-3 text-white rounded md:p-0 dark:text-white">
-                                    Clientes
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#contact"
-                                    className="block py-2 px-3 text-white rounded md:p-0 dark:text-white">
-                                    Contacto
-                                </a>
+                                <a href="#contact" className="block py-2 px-3 text-white rounded md:p-0">Contacto</a>
                             </li>
                         </ul>
                     </div>
